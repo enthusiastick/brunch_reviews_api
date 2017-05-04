d898f345f7ae765ebffaecf96a9105451aa971c5
Eben <eben.lowe@gmail.com>
2017-05-04 14:36:48 -0400

    Set up faker seed.

--- Gemfile
+++ Gemfile
@@ -8,6 +8,7 @@ end
 gem "active_model_serializers", "~> 0.10.0"
 gem "bcrypt", "~> 3.1.7"
 gem "coffee-rails", "~> 4.2"
+gem "faker"
 gem "jbuilder", "~> 2.5"
 gem "pg", "~> 0.18"
 gem "puma", "~> 3.7"
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,24 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+user = User.find_or_initialize_by(email: "foob@example.com", handle: "foob")
+if !user.persisted?
+  user.password = "password"
+  user.save
+end
+
+10.times do
+  user = User.find_or_initialize_by(email: Faker::Internet.safe_email)
+  if !user.persisted?
+    user.handle = Faker::Internet.user_name
+    user.password = "password"
+    user.save
+  end
+end
+
+3.times do
+  Venue.find_or_create_by(creator: user, description: Faker::Commerce.department(5), location: Faker::Address.street_address, name: Faker::Company.name)
+end
+
+Venue.all.each do |venue|
+  10.times do
+    Review.create(body: Faker::Hipster.paragraph(2), rating: rand(4) + 1, user: User.all.sample, venue: venue)
+  end
+end
